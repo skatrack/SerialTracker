5881d75d008aaf7e12e61fa9099cffbbbb409f49
Yury Vostrenkov <yuvostrenkov@gmail.com>
2019-06-24 23:25:29 +0300

    float management changed for protocol

--- inc/serial.h
+++ inc/serial.h
@@ -6,11 +6,11 @@
 
 typedef struct
 {
-  uint16_t  Begin  ;   	// 2  	0xAAAA
-  float    gyro[3];   	// 12  	gyro
-  float    accel[3];    // 12 	accel
-	float 	 timestamp;		// 4 		timestamp
-  uint16_t  End ;      	// 2  	0x5555
+  uint16_t  Begin;	   	// 2  	0xAAAA
+  float     gyro[3];   	// 12  	gyro
+  float     accel[3];   // 12 	accel
+	float 	  timestamp;	// 4 		timestamp
+  uint16_t  End;      	// 2  	0x5555
 } 
 data_t;
 
--- src/serial.c
+++ src/serial.c
@@ -15,15 +15,50 @@ void SerialSetup()
 
 void SerialSendPacket()
 {
-  hat.gyro[0]=gyro[0];
-  hat.gyro[1]=gyro[1];
-  hat.gyro[2]=gyro[2];
-	hat.accel[0]=accel[0];
-  hat.accel[1]=accel[1];
-  hat.accel[2]=accel[2];
-	hat.timestamp = uptime;
+	uint8_t buffer[32];
+	uint8_t i=0;
+	buffer[i++]=0xAA;
+	buffer[i++]=0xAA;
 	
-  UART1_send_bytes( (uint8_t*) &hat,sizeof(hat));
+	buffer[i++]= (*(uint32_t*) &gyro[0]);
+	buffer[i++]= (*(uint32_t*) &gyro[0])>>8;
+	buffer[i++]= (*(uint32_t*) &gyro[0])>>16;
+  buffer[i++]= (*(uint32_t*) &gyro[0]) >> 24;
+
+	buffer[i++]= (*(uint32_t*) &gyro[3]);
+	buffer[i++]= (*(uint32_t*) &gyro[2])>>8;
+	buffer[i++]= (*(uint32_t*) &gyro[1])>>16;
+  buffer[i++]= (*(uint32_t*) &gyro[1])>>24;
+	
+	buffer[i++]= (*(uint32_t*) &gyro[2]);
+	buffer[i++]= (*(uint32_t*) &gyro[2])>>8;
+	buffer[i++]= (*(uint32_t*) &gyro[2])>>16;
+  buffer[i++]= (*(uint32_t*) &gyro[2])>>24;
+	
+	buffer[i++]= (*(uint32_t*) &accel[0]);
+	buffer[i++]= (*(uint32_t*) &accel[0])>>8;
+	buffer[i++]= (*(uint32_t*) &accel[0])>>16;
+	buffer[i++]= (*(uint32_t*) &accel[0])>>24;
+	
+	buffer[i++]= (*(uint32_t*) &accel[3]);
+	buffer[i++]= (*(uint32_t*) &accel[2])>>8;
+	buffer[i++]= (*(uint32_t*) &accel[1])>>16;
+  buffer[i++]= (*(uint32_t*) &accel[1])>>24;
+	
+	buffer[i++]= (*(uint32_t*) &accel[2]);
+	buffer[i++]= (*(uint32_t*) &accel[2])>>8;
+	buffer[i++]= (*(uint32_t*) &accel[2])>>16;
+  buffer[i++]= (*(uint32_t*) &accel[2])>>24;
+	
+	buffer[i++]= (*(uint32_t*) &uptime);
+	buffer[i++]= (*(uint32_t*) &uptime)>>8;
+	buffer[i++]= (*(uint32_t*) &uptime)>>16;
+	buffer[i++]= (*(uint32_t*) &uptime)>>24;
+	
+	buffer[i++]=0x55;
+	buffer[i++]=0x55;
+	
+  UART1_send_bytes( (uint8_t*) &buffer, sizeof(buffer));
 }
 
 
